96d1ff28ae443be12daf217d69858658b8fd6b57
Raymond Ariwoola <Raymondariwoola@gmail.com>
2021-09-10 23:24:39 +0400

    Level 6 - Google OAuth 2.0 Authentication

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 // all .js and .mjs files are interpreted as ES modules
-import { } from 'dotenv/config';
-import express from 'express'
+import { } from "dotenv/config";
+import express from "express"
 import bodyParser from "body-parser"
 import ejs from "ejs"
 import mongoose from "mongoose"
 // import bcrypt from "bcrypt"
 // import encrypt from "mongoose-encryption" // Cipher encryption
 // import md5 from "md5" // MD5 Hash Algorithm
-import session from 'express-session'
-import passport from 'passport'
+import session from "express-session"
+import passport from "passport"
 import passportLocalMongoose from "passport-local-mongoose"
+import { Strategy as GoogleStrategy } from "passport-google-oauth20"
+import findOrCreate from "mongoose-findorcreate"
 
 
 const app = express();
@@ -35,25 +37,76 @@ app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
+const userPosts = new mongoose.Schema({
+    postID: String,
+    postContent: String
 });
 
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    googleId: String,
+    displayName: String,
+    familyName: String,
+    givenName: String,
+    profilePhoto: String,
+    posts: [userPosts]
+});
+
+
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-// use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
 
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
+
+        const userProfileInfo = {
+            displayName: profile.displayName,
+            profilePhoto: profile.photos[0].value,
+            familyName: profile.name.familyName,
+            givenName: profile.name.givenName
+        }
+
+        User.findOrCreate({ googleId: profile.id }, userProfileInfo, function (err, user) {
+            console.log(user);
+            return cb(err, user);
+        });
+    }
+));
 
 app.get("/", function (req, res) { // handles GET request for home route
     res.render('home');
 });
+
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect to secrets page.
+        res.redirect('/secrets');
+    });
 
 app.get("/login", function (req, res) { // handles GET request for login route
     res.render('login');
@@ -65,7 +118,11 @@ app.get("/register", function (req, res) { // handles GET request for register r
 
 app.get("/secrets", function (req, res) { // handles GET request for secrets route
     if (req.isAuthenticated()) { //! if user is already authenticated then they should have access to the page
-        res.render("secrets");
+        // req.user._id 
+        // req.user.googleId 
+        // req.user.displayName
+
+        res.render("secrets", { profilePhoto: req.user.profilePhoto, displayName: req.user.displayName });
     } else { //! if user is not already authenticated, then they should be routed to the login page
         res.redirect("/login");
     }
@@ -78,12 +135,11 @@ app.get("/logout", function (req, res) { // handles GET request for Log out rout
 
 
 app.post("/register", function (req, res) {
-
     User.register({ username: req.body.username }, req.body.password, function (err, user) {
         if (err) {
             console.log(err);
-            res.send(err);
-            // res.redirect("/register");
+            // res.send(err);
+            res.redirect("/register");
         } else {
             passport.authenticate("local")(req, res, function () {
                 res.redirect("/secrets");
